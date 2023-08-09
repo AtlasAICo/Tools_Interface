28915fe0abe48f761825b5360da9fc5deee390c8
Govind Kumar <gk4051668@gmail.com>
2023-08-09 08:35:17 +0530

    added initial commit

---
+++ src/components/user_info/ToolsInput.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { Input, Text, Box, Textarea } from "@chakra-ui/react";
+
+const ToolsInput = ({ query, setQuery, systemMessage, setSystemMessage }) => {
+  return (
+    <Box p={"2"}>
+      <Text fontSize={"2xl"}>Prompt</Text>
+      <Textarea
+        size={"md"}
+        maxH={"30vh"}
+        p={"20px"}
+        placeholder="Enter the prompt here..."
+        value={systemMessage}
+        textAlign={"justify"}
+        onChange={(e) => setSystemMessage(e.target.value)}
+      />
+      <Text fontSize={"2xl"}>Tools Input</Text>
+      <Input
+        placeholder="Enter the query"
+        variant="outline"
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+      />
+    </Box>
+  );
+};
+
+export default ToolsInput;
---
+++ src/components/user_info/ToolsOutput.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { Box, Text, Textarea } from "@chakra-ui/react";
+
+const ToolsOutput = ({ output }) => {
+  return (
+    <Box maxH={"50vh"} p={"2"} textAlign={"justify"} m={"1"}>
+      <Text fontSize={"2xl"}>Tools Output</Text>
+      <Textarea
+        size={"md"}
+        maxH={"30vh"}
+        placeholder="Output will appear here..."
+        value={output}
+        textAlign={"justify"}
+        readOnly={true}
+      />
+    </Box>
+  );
+};
+
+export default ToolsOutput;
